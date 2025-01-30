06095d80c1a09b2925f9d4b7d8268f19ec10323a
mobin afshari <mma137813781378@gmail.com>
2025-01-30 12:34:38 +0330

    working on bubble sort

--- Insertion-sort/index.js
+++ Insertion-sort/index.js
@@ -1,9 +1,9 @@
-const arr = [4, 2, 3, 1 , 9 , 2 , 2 , 0 , -1];
+const arr = [4, 2, 3, 1, 9, 2, 2, 0, -1];
 let current;
 for (let i = 1; i < arr.length; i++) {
-    current = arr[i];
-    let j = i - 1;
-    while (arr[j] > current) {
+  current = arr[i];
+  let j = i - 1;
+  while (arr[j] > current) {
     arr[j + 1] = arr[j];
     j--;
   }
