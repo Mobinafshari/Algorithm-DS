5ba5b3db4f6c9d001fb790d869f21787f2e3254e
mobin afshari <mma137813781378@gmail.com>
2025-02-25 22:01:13 +0330

    fixx: linked lisst implementation refactored and started from scratch

--- Linked-list/index.js
+++ Linked-list/index.js
@@ -7,55 +7,52 @@ class Node {
 
 class Linkedlist {
   constructor() {
-    this.list = [];
-    
+    this.head = null;
+    this.size = 0;
   }
   append(value) {
-    let length = this.list.length;
     const node = new Node(value);
+    this.size++;
+
     if (this.isEmpty()) {
-      return (this.list[length] = node);
+      this.head = node;
+      return;
     }
-    this.list[length] = node;
-    this.list[length - 1].next = this.list[length];
+
+    let current = this.head;
+    while (current.next !== null) {
+      current = current.next; 
+    }
+
+    current.next = node; 
   }
   prepend(value) {
-    let length = this.list.length;
+    const node = new Node(value);
+    this.size++;
     if (this.isEmpty()) {
-      const emptyNode = new Node(value);
-      return (this.list[length] = emptyNode);
+      return (this.head = node);
     }
-    const node = new Node(value, this.list[0]);
-    this.list = [node, ...this.list];
+    node.next = this.head;
+    return (this.head = node);
   }
   insertAt(value, index) {
-    let length = this.list.length;
-    if (index > length) throw Error("index is greater than list length");
-    if (index === length) return this.append(value);
+    if (index === this.size) return this.append(value);
     if (index === 0) return this.prepend(value);
-    let newList = [];
-    for (let i = 0; i < index; i++) {
-      newList[newList.length] = this.list[i];
-    }
-    const node = new Node(value, this.list[index]);
-    newList[newList.length] = node;
-    newList[newList.length - 2].next = newList[newList.length - 1];
-    for (let j = index; j < length; j++) {
-      newList[newList.length] = this.list[j];
-    }
-    this.list = newList;
+    
   }
   isEmpty() {
-    return this.list.length === 0;
+    return this.head === null;
   }
 }
 
 const l = new Linkedlist();
 
-l.prepend(10);
 l.append(20);
-l.append(30);
-l.insertAt(49, 0);
-l.prepend(100);
-l.insertAt(1000, 2);
-console.log(l.list);
+l.append(10);
+l.prepend(30);
+// l.append(30);
+l.insertAt(49, 3);
+// l.prepend(100);
+// l.insertAt(1000, 2);
+console.log(l.head);
+console.log(l.size);
