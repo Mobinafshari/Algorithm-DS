b12efb518b918c12ae6bcf91a1a780df46c408d0
mobin afshari <mma137813781378@gmail.com>
2025-02-25 19:48:18 +0330

    fix: Node class added to the linked list

--- Linked-list/index.js
+++ Linked-list/index.js
@@ -1,21 +1,31 @@
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
 class Linkedlist {
   constructor() {
     this.list = [];
   }
   append(value) {
     let length = this.list.length;
-    if (!this.isEmpty()) {
-      this.list[length] = { value, next: null };
-      this.list[length - 1].next = this.list[length];
+    const node = new Node(value);
+    if (this.isEmpty()) {
+      return (this.list[length] = node);
     }
-    this.list[length] = { value, next: null };
+    this.list[length] = node;
+    this.list[length - 1].next = this.list[length];
   }
   prepend(value) {
     let length = this.list.length;
     if (this.isEmpty()) {
-      return (this.list[length] = { value, next: null });
+      const emptyNode = new Node(value);
+      return (this.list[length] = emptyNode);
     }
-    this.list = [{ value, next: this.list[0] }, ...this.list];
+    const node = new Node(value, this.list[0]);
+    this.list = [node, ...this.list];
   }
   insertAt(value, index) {
     let length = this.list.length;
@@ -26,12 +36,12 @@ class Linkedlist {
     for (let i = 0; i < index; i++) {
       newList[newList.length] = this.list[i];
     }
-    newList[newList.length] = { value, next: this.list[index] };
+    const node = new Node(value, this.list[index]);
+    newList[newList.length] = node;
     newList[newList.length - 2].next = newList[newList.length - 1];
     for (let j = index; j < length; j++) {
       newList[newList.length] = this.list[j];
     }
-    console.log("====>>>>>", newList, newList.length);
     this.list = newList;
   }
   isEmpty() {
@@ -40,7 +50,6 @@ class Linkedlist {
 }
 
 const l = new Linkedlist();
-
 
 l.prepend(10);
 l.append(20);
