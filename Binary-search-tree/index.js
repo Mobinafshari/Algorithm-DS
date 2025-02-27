f9452348fe0b4f853f82cf712f355c0020bf44c5
mobin afshari <mma137813781378@gmail.com>
2025-02-27 11:55:32 +0330

    feat: add method added

--- Binary-search-tree/index.js
+++ Binary-search-tree/index.js
@@ -11,14 +11,42 @@ class BinarySearchTree {
     this.tree = null;
   }
   add(value) {
-    let node = new Node(value);
+    const node = new Node(value);
+
     if (this.tree === null) {
       this.tree = node;
       return;
+    }
+
+    let current = this.tree;
+
+    while (true) {
+      if (value > current.data) {
+        if (current.right === null) {
+          current.right = node;
+          return;
+        }
+        current = current.right;
+      } else if (value < current.data) {
+        if (current.left === null) {
+          current.left = node;
+          return;
+        }
+        current = current.left;
+      } else {
+        return;
+      }
     }
   }
 }
 
 const list = new BinarySearchTree();
 list.add(20);
+list.add(30);
+list.add(10);
+list.add(100);
+list.add(25);
+list.add(5);
+list.add(500);
+list.add(15);
 console.log(list.tree);
