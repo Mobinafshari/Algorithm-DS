1e226042913166e0dcf2b56f7cebe3ec5aeb6ec3
Mobinafshari <mma137813781378@gmail.com>
2025-05-21 22:29:10 +0330

    fix: ds and methods implemented

--- Topological-sort/index.js
+++ Topological-sort/index.js
@@ -1,5 +1,38 @@
-class DAG {
-    constructor(){
-        this.graph = null
+class Node {
+    constructor(name){
+        this.name = name;
+        this.next = []
     }
 }
+
+class DAG {
+    constructor(){
+        this.nodes = new Map();
+    }
+    addNode(name){
+        if(this.nodes.has(name)) return;
+        this.nodes.set(name , new Node(name))
+    }
+    addEdge(from , to){
+        if(!this.getNode(from)){
+            this.addNode(from)
+        } if(!this.getNode(to)){
+            this.addNode(to);
+        }
+        this.getNode(from).next.push(this.getNode(to))
+    }
+    getNode(name){
+        return this.nodes.get(name)
+    }
+    getAllNodes(){
+        return [...this.nodes.values()]
+    }
+}
+
+const dag = new DAG();
+
+dag.addEdge("A", "C");
+dag.addEdge("B", "C");
+dag.addEdge("C", "D");
+
+console.log(dag.getAllNodes())
