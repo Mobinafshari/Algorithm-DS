c76c1ade083a4e772dc32d5b97a75b94c72dcd33
Mobinafshari <mma137813781378@gmail.com>
2025-05-22 14:45:13 +0330

    fixdfs Topological sort implemented

--- Topological-sort/index.js
+++ Topological-sort/index.js
@@ -27,14 +27,52 @@ class DAG {
     getAllNodes(){
         return [...this.nodes.values()]
     }
+    getStartedNode(){
+        const nodeNames = Array.from(this.nodes.keys());
+        const hasInputNodes = new Set();
+        this.nodes.forEach((node) => 
+            node.next.forEach((next) => {
+                hasInputNodes.add(next.name)
+            })
+        )
+        return nodeNames.find((node) => !Array.from(hasInputNodes).includes(node))
+        
+    }
 }
 function topologicalSort(graph) {
-    
+    const visited = new Set();
+    const result = [];
+    const visiting = new Set(); 
+
+    function dfs(node) {
+        if (visiting.has(node.name)) {
+            throw new Error(`Cycle detected at node ${node.name}`);
+        }
+
+        if (visited.has(node.name)) return;
+
+        visiting.add(node.name);
+        for (const neighbor of node.next) {
+            dfs(neighbor);
+        }
+        visiting.delete(node.name);
+        visited.add(node.name);
+        result.push(node.name);
+    }
+
+    for (const node of graph.getAllNodes()) {
+        dfs(node);
+    }
+
+    return result.reverse(); 
 }
+
+
 const dag = new DAG();
 
 dag.addEdge("A", "C");
+dag.addEdge("A", "B");
 dag.addEdge("B", "C");
+dag.addEdge("B", "E");
 dag.addEdge("C", "D");
-
-console.log(dag.getAllNodes())
+console.log(topologicalSort(dag))
