e803a452c2d74b3e8dc42bbbe544b055b55a2f38
Mobinafshari <mma137813781378@gmail.com>
2025-05-31 21:51:01 +0330

    fix: working on bfs algorithm

--- BFS/index.js
+++ BFS/index.js
@@ -1,34 +1,18 @@
-function bfs(graph, startNode) {
-  let iteratedNodes = [];
-
-  function pop(arr = []) {
-    let firstChild = arr.shift();
-    if (firstChild) {
-      arr.push(
-        ...graph[firstChild].filter(
-          (ele) => !iteratedNodes.includes(ele) && !arr.includes(ele)
-        )
-      );
-      iteratedNodes.push(firstChild);
-    }
-    console.log("=>", arr);
-  }
-
-  let array = [startNode];
-  while (array.length > 0) {
-    pop(array);
-  }
-
-  console.log("BFS Traversal Order:", iteratedNodes);
-}
-
 const graph = {
   A: ["B", "C"],
-  B: ["A", "D", "E"],
-  C: ["A", "F"],
-  D: ["B"],
-  E: ["B", "F"],
-  F: ["C", "E"],
+  B: ["D", "E"],
+  C: ["F"],
+  D: [],
+  E: ["F"],
+  F: []
 };
 
-bfs(graph, "A");
+
+function bfs(graph, start) {
+  const visited = new Set();
+  const queue = [];
+
+  
+
+  return Array.from(visited); 
+}
