af48104618016f96636def105055bc995f777db2
Mobinafshari <mma137813781378@gmail.com>
2025-05-31 21:49:09 +0330

    fix: implemented dfs again ;)

--- DFS/index.js
+++ DFS/index.js
@@ -1,34 +1,28 @@
-function dfs(graph, startNode) {
-  let iteratedNodes = [];
-
-  function pop(arr = []) {
-    let lastChild = arr.pop();
-    if (lastChild) {
-      arr.push(
-        ...graph[lastChild]
-          .filter((ele) => !iteratedNodes.includes(ele) && !arr.includes(ele))
-          .reverse() 
-      );
-      iteratedNodes.push(lastChild);
-    }
-    console.log("=>", arr);
-  }
-
-  let array = [startNode];
-  while (array.length > 0) {
-    pop(array);
-  }
-
-  console.log("DFS Traversal Order:", iteratedNodes);
-}
-
 const graph = {
   A: ["B", "C"],
-  B: ["A", "D", "E"],
-  C: ["A", "F"],
-  D: ["B"],
-  E: ["B", "F"],
-  F: ["C", "E"],
+  B: ["D", "E"],
+  C: ["F", "G"],
+  D: ["H"],
+  E: [],
+  F: ["I"],
+  G: [],
+  H: [],
+  I: []
 };
 
-dfs(graph, "A");
+function dfs(graph, start) {
+  const visited = new Set();
+
+  function visit(node) {
+    if (visited.has(node)) return;
+    visited.add(node);
+    for (const neighbor of graph[node]) {
+      visit(neighbor);
+    }
+  }
+
+  visit(start);
+  return Array.from(visited);
+}
+
+console.log(dfs(graph, 'A'));
