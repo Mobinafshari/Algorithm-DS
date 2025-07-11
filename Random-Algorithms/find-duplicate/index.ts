a8d7206f971ce3fc866eecbf29cd12cbee417da0
Mobinafshari <mma137813781378@gmail.com>
2025-07-11 20:40:40 +0330

    fix: implemented normal way

--- Random-Algorithms/find-duplicate/index.ts
+++ Random-Algorithms/find-duplicate/index.ts
@@ -1,6 +1,15 @@
+// function findDuplicate(arr: number[]) {
+//   const uniqueOnes = new Set(arr);
+//   return ![...uniqueOnes].length === arr.length;
+// }
+
 function findDuplicate(arr: number[]) {
-  const uniqueOnes = new Set(arr);
-  return [...uniqueOnes].length === arr.length;
+  const seen = new Set<number>();
+  for (const num of arr) {
+    if (seen.has(num)) return true;
+    seen.add(num);
+  }
+  return false;
 }
 
-console.log(findDuplicate([1, 2, 4]));
+console.log(findDuplicate([3, 2, 6, 5, 0, 3, 10, 3, 10, 5]));
