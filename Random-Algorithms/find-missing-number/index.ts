e3ab806f5106def36c0d85d6b53ae6695176ceb9
Mobinafshari <mma137813781378@gmail.com>
2025-07-12 11:22:44 +0330

    fix: implemented in first way

--- Random-Algorithms/find-missing-number/index.ts
+++ Random-Algorithms/find-missing-number/index.ts
@@ -1,5 +1,10 @@
-function findMissingNumberInSequence(numbers: number[]): number {
-  return 2;
+function findMissingNumberInSequence(numbers: number[]): number | undefined {
+  const options = Array.from({ length: numbers.length + 1 }).map((_, i) => i);
+  const nums = new Set(numbers);
+  for (const num of options) {
+    if (nums.has(num)) continue;
+    return num;
+  }
 }
 
 console.log(findMissingNumberInSequence([3, 0, 4, 2, 1]));
