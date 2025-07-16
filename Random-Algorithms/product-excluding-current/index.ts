1983dfd3a9ff5c8837dfdfc93b1d1dedcc3fd0d9
Mobinafshari <mma137813781378@gmail.com>
2025-07-16 21:53:01 +0330

    fix: simple version

--- Random-Algorithms/product-excluding-current/index.ts
+++ Random-Algorithms/product-excluding-current/index.ts
@@ -1,9 +1,12 @@
 function arrayProductExcludingCurrent(numbers: number[]): number[] {
-  const result = [];
-  for (let i = 0; i < numbers.length; i++){
-    
-  } 
-  return;
+  const result: number[] = [];
+  for (let i = 0; i < numbers.length; i++) {
+    const number = numbers[i];
+    for (let j = i + 1; j < numbers.length; j++) {
+      result.push(number * numbers[j]);
+    }
+  }
+  return result;
 }
 
 console.log(arrayProductExcludingCurrent([1, 2, 3]));
