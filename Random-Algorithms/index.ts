791ea54ca8f5cbf989d3129aba9fd8b8e7a54847
Mobinafshari <mma137813781378@gmail.com>
2025-07-16 21:40:24 +0330

    feat: working on random algorithms everyday

--- Random-Algorithms/index.ts
+++
@@ -1,3 +0,0 @@
-function mostCommonElements(numbers: number[], k: number): number[] {}
-
-console.log(mostCommonElements([4, 4, 4, 6, 6, 5, 5, 5], 2));
---
+++ Random-Algorithms/product-excluding-current/index.ts
@@ -0,0 +1,9 @@
+function arrayProductExcludingCurrent(numbers: number[]): number[] {
+  const result = [];
+  for (let i = 0; i < numbers.length; i++){
+    
+  } 
+  return;
+}
+
+console.log(arrayProductExcludingCurrent([1, 2, 3]));
