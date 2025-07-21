90f545dc0b8ccf04500e7a697d5e0b24ec847592
Mobinafshari <mma137813781378@gmail.com>
2025-07-21 20:52:10 +0330

    feat: insertAt method implemented

--- Linked-list/index.ts
+++ Linked-list/index.ts
@@ -45,10 +45,38 @@ class LinkedList<T> {
     this.length++;
     return this;
   }
+  insertAt(value: T, index: number): this {
+    if (index === 0) {
+      this.prepend(value);
+      return this;
+    } else if (index === this.length) {
+      this.append(value);
+      return this;
+    } else if (index < 0 || index > this.length) {
+      throw Error("Index must be less than list length");
+    } else {
+      const node = new CustomNode(value);
+      let current = this.head;
+      let prev: CustomNode<T> | null = null;
+      let i = 0;
+
+      while (i < index) {
+        prev = current;
+        current = current!.next;
+        i++;
+      }
+
+      prev!.next = node;
+      node.next = current;
+      this.length++;
+      return this;
+    }
+  }
 }
 
 const list = new LinkedList<number>();
 list.append(5);
 list.append(1000);
 list.append(120);
-console.log(list.prepend(1))
+list.prepend(1);
+console.log(list.insertAt(55, 1));
