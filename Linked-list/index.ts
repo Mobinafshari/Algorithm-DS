98d66dfb4f360410c145ef1d431d356dd4ecf965
Mobinafshari <mma137813781378@gmail.com>
2025-07-21 20:16:54 +0330

    fix: couple of  methods implemented

--- Linked-list/index.ts
+++ Linked-list/index.ts
@@ -7,8 +7,47 @@ class CustomNode<T> {
 }
 
 class LinkedList<T> {
-  private head: CustomNode<T> | null;
+  public head: CustomNode<T> | null;
   private tail: CustomNode<T> | null;
   private length: number;
-  constructor() {}
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  private isEmpty(): boolean {
+    return this.length === 0;
+  }
+  append(value: T): void {
+    const node = new CustomNode(value);
+    if (this.isEmpty()) {
+      this.head = node;
+      this.tail = node;
+      this.length++;
+      return;
+    }
+    this.tail!.next = node;
+    this.tail = node;
+    this.length++;
+  }
+  prepend(value: T): void {
+    const node = new CustomNode(value);
+
+    if (this.isEmpty()) {
+      this.head = node;
+      this.tail = node;
+      this.length++;
+      return;
+    }
+    node.next = this.head;
+    this.head = node;
+    this.length++;
+  }
 }
+
+const list = new LinkedList();
+list.append(5);
+list.append("hashem");
+list.append(120);
+list.prepend(1)
+console.log(list.head);
