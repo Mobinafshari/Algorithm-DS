1948c15ea1d88946ac0544581d041285d80c228d
Mobinafshari <mma137813781378@gmail.com>
2025-07-24 13:10:07 +0330

    fix: implemented couple of methods

--- Stack-ds/index.ts
+++ Stack-ds/index.ts
@@ -1,3 +1,26 @@
-class Stack {
-    
+class Stack<T> {
+  public stack: T[];
+  constructor() {
+    this.stack = [];
+  }
+
+  push(value: T): this {
+    this.stack[this.stack.length] = value;
+    return this;
+  }
+  pop(): T | undefined {
+    if (this.isEmpty()) return undefined;
+    let topElement = this.stack[this.stack.length - 1];
+    this.stack.length--;
+    return topElement;
+  }
+  private isEmpty(): boolean {
+    return this.stack.length === 0;
+  }
 }
+
+const ds = new Stack<number>();
+ds.push(3);
+ds.push(5);
+console.log(ds.pop());
+console.log(ds.stack);
