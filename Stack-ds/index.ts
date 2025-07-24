c3b93528ffa7296509e4c36a6856a8bad97a329a
Mobinafshari <mma137813781378@gmail.com>
2025-07-24 16:35:52 +0330

    feat: working on random algorithms on stack DS

--- Stack-ds/index.ts
+++ Stack-ds/index.ts
@@ -17,10 +17,31 @@ class Stack<T> {
   private isEmpty(): boolean {
     return this.stack.length === 0;
   }
+  peek(): T | undefined {
+    if (this.isEmpty()) return undefined;
+    return this.stack[this.stack.length - 1];
+  }
+  isFull(maxSize: number): boolean {
+    return this.stack.length === maxSize;
+  }
+  getMin(): number | undefined {
+    if (this.isEmpty()) return undefined;
+    let min = Infinity;
+    for (const num of this.stack as number[]) {
+      min = min > num ? num : min;
+    }
+    return min;
+  }
 }
+
+function asteroidCollision(asteroids: number[]): number[] {
+    
+};
 
 const ds = new Stack<number>();
 ds.push(3);
 ds.push(5);
-console.log(ds.pop());
-console.log(ds.stack);
+ds.push(2);
+ds.push(1);
+ds.push(50);
+console.log(ds.getMin());
