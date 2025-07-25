02589b26136a5fd945a0c10fed8640839c01691e
Mobinafshari <mma137813781378@gmail.com>
2025-07-25 11:39:33 +0330

    fix: some implementation for array added(4)

--- Random-Algorithms/Arrays/index.ts
+++ Random-Algorithms/Arrays/index.ts
@@ -93,4 +93,31 @@ function containsDuplicate(nums: number[]): boolean {
   const set = new Set(nums);
   return set.size !== nums.length;
 }
-console.log(containsDuplicate([1, 2, 3, 1]));
+function search(nums: number[], target: number): number {
+  let left = 0,
+    right = nums.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (nums[mid] === target) return mid;
+
+    if (nums[left] <= nums[mid]) {
+      if (nums[left] <= target && target < nums[mid]) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    }
+    else {
+      if (nums[mid] < target && target <= nums[right]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+  }
+
+  return -1;
+}
+
+console.log(search([4, 5, 6, 7, 0, 1, 2], 0));
