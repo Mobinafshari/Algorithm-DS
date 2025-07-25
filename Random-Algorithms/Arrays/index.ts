5282cf0c21ab06d94cb50ac2a2b52cf91986972c
Mobinafshari <mma137813781378@gmail.com>
2025-07-25 13:37:24 +0330

    fix: some implementation for array added(5)

--- Random-Algorithms/Arrays/index.ts
+++ Random-Algorithms/Arrays/index.ts
@@ -107,8 +107,7 @@ function search(nums: number[], target: number): number {
       } else {
         left = mid + 1;
       }
-    }
-    else {
+    } else {
       if (nums[mid] < target && target <= nums[right]) {
         left = mid + 1;
       } else {
@@ -120,4 +119,33 @@ function search(nums: number[], target: number): number {
   return -1;
 }
 
-console.log(search([4, 5, 6, 7, 0, 1, 2], 0));
+function threeSum(nums: number[]): number[][] {
+  const res: number[][] = [];
+  nums.sort((a, b) => a - b);
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+    let left = i + 1;
+    let right = nums.length - 1;
+
+    while (left < right) {
+      const sum = nums[i] + nums[left] + nums[right];
+      if (sum === 0) {
+        res.push([nums[i], nums[left], nums[right]]);
+        left++;
+        right--;
+        while (left < right && nums[left] === nums[left - 1]) left++;
+        while (left < right && nums[right] === nums[right + 1]) right--;
+      } else if (sum < 0) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  }
+
+  return res;
+}
+
+console.log(threeSum([-1, 0, 1, 2, -1, -4]));
