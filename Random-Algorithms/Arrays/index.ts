c228895ccfd97e83393492df183ef01790db27c0
Mobinafshari <mma137813781378@gmail.com>
2025-07-27 23:04:26 +0330

    fix: re-implementing stuff

--- Random-Algorithms/Arrays/index.ts
+++ Random-Algorithms/Arrays/index.ts
@@ -114,7 +114,17 @@ function maxProfit(prices: number[]): number {
 // }
 function productExceptSelf(nums: number[]): number[] {
   let res: number[] = new Array(nums.length).fill(null);
-  console.log(res);
+  let prefix = 1;
+  for (let i = 0; i < nums.length; i++) {
+    res[i] = prefix;
+    prefix *= nums[i];
+  }
+  let suffix = 1;
+  for (let i = nums.length - 1; i >= 0; i--) {
+    res[i] *= suffix;
+    suffix *= nums[i];
+  }
+  return res;
 }
 console.log(productExceptSelf([1, 2, 3, 4]));
 
