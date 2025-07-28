dd150707b83c1fb181313da15d007e735de33d38
Mobin Afshari <mma137813781378@gmail.com>
2025-07-28 11:30:38 +0330

    fix: re implement bfs again

--- BFS/index.js
+++ BFS/index.js
@@ -8,29 +8,46 @@ const graph = {
 };
 
 
+// function bfs(graph, start) {
+//   const visited = new Set();
+//   const queue = [];
+
+//   function visit(node) {
+//     if (visited.has(node)) return;
+//     console.log('===>', node);
+//     visited.add(node);
+//     for (const neighbor of graph[node]) {
+//       if (!visited.has(neighbor)) {
+//         queue.push(neighbor);
+//       }
+//     }
+//   }
+
+//   visit(start);
+
+//   while (queue.length !== 0) {
+//     const nextNode = queue.shift();
+//     visit(nextNode);
+//   }
+
+//   return Array.from(visited);
+// }
 function bfs(graph, start) {
   const visited = new Set();
-  const queue = [];
+  const queue = [start];
+  visited.add(start); 
 
-  function visit(node) {
-    if (visited.has(node)) return;
-    console.log('===>', node);
-    visited.add(node);
-    for (const neighbor of graph[node]) {
+  while (queue.length !== 0) {
+    const node = queue.shift();
+    for (const neighbor of graph[node] || []) {
       if (!visited.has(neighbor)) {
+        visited.add(neighbor);
         queue.push(neighbor);
       }
     }
   }
 
-  visit(start);
-
-  while (queue.length !== 0) {
-    const nextNode = queue.shift();
-    visit(nextNode);
-  }
-
-  return Array.from(visited);
+  return [...visited];
 }
 
 console.log(bfs(graph , 'A'))
