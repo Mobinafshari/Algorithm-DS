735a348b02566a23f110560d2acec5d9d099068a
Mobinafshari <mma137813781378@gmail.com>
2025-07-29 13:59:57 +0330

    fix: re-implmenting stuff on strings

--- Random-Algorithms/String/index.ts
+++ Random-Algorithms/String/index.ts
@@ -1,21 +1,37 @@
+// function countSubstrings(s: string): number {
+//   let count = 0;
+
+//   const expand = (left: number, right: number) => {
+//     while (left >= 0 && right < s.length && s[left] === s[right]) {
+//       count++;
+//       left--;
+//       right++;
+//     }
+//   };
+
+//   for (let i = 0; i < s.length; i++) {
+//     expand(i, i);
+//     expand(i, i + 1);
+//   }
+
+//   return count;
+// }
 function countSubstrings(s: string): number {
   let count = 0;
-
-  const expand = (left: number, right: number) => {
+  function checker(left: number, right: number) {
     while (left >= 0 && right < s.length && s[left] === s[right]) {
       count++;
       left--;
       right++;
     }
-  };
-
-  for (let i = 0; i < s.length; i++) {
-    expand(i, i);
-    expand(i, i + 1);
   }
-
+  for (let i = 0; i < s.length; i++) {
+    checker(i, i);
+    checker(i, i + 1);
+  }
   return count;
 }
+console.log(countSubstrings("abba"));
 
 function isValid(s: string): boolean {
   const stack: string[] = [];
@@ -37,4 +53,3 @@ function isValid(s: string): boolean {
   return stack.length === 0;
 }
 
-console.log(isValid("()"));
