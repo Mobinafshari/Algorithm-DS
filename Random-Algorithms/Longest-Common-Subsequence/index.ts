da9b482d4236ac817833a864e21f6c4cd6fcbfcb
Mobinafshari <mma137813781378@gmail.com>
2025-08-08 15:58:21 +0330

    fix: implemented with DP

--- Random-Algorithms/Longest-Common-Subsequence/index.ts
+++ Random-Algorithms/Longest-Common-Subsequence/index.ts
@@ -1,6 +1,19 @@
 function longestCommonSubsequence(text1: string, text2: string): number {
   if (text1 === text2) return text1.length;
+  let m = text1.length;
+  let n = text2.length;
 
+  let dp = Array.from(Array(m + 1), () => new Array(n + 1).fill(0));
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (text1[i - 1] === text2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+      }
+    }
+  }
+  return dp[m][n];
   // const textOneSequences = generateSubsequences(text1);
   // const textTwoSequences = generateSubsequences(text2);
   // let result = 0;
