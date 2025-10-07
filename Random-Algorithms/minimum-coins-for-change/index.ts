a4d64da69783757a98baa77e314346396fa0a92a
Mobin Afshari <mma137813781378@gmail.com>
2025-10-07 21:23:50 +0330

    feat: working on random algorithm

--- Random-Algorithms/minimum-coins-for-change/index.ts
+++ Random-Algorithms/minimum-coins-for-change/index.ts
@@ -1,5 +1,16 @@
-export default function minimumCoinsForChange(coins, target) {
-  throw "Not implemented!";
+export default function minimumCoinsForChange(coins: number[], target: number) {
+  if (target == 0) return 0;
+  let min = Infinity;
+  for (const number of coins) {
+    if (target % number == 0) {
+      min = Math.min(min, target / number);
+      continue;
+    } else if (coins.includes(number % target)) {
+      console.log("hello");
+      min = Math.min(min, (target - (target % number)) / number + 1);
+    }
+  }
+  return min == Infinity ? -1 : min;
 }
 
-console.log(minimumCoinsForChange([3, 7, 4], 14));
+console.log(minimumCoinsForChange([2], 3));
