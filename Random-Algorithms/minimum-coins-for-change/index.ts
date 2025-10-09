ca48cd0de4bc60ff361f57ba22fefb3186e6de87
Mobin Afshari <mma137813781378@gmail.com>
2025-10-09 17:07:40 +0330

    fix: minimum coins for change implemented by dynamic programming

--- Random-Algorithms/minimum-coins-for-change/index.ts
+++ Random-Algorithms/minimum-coins-for-change/index.ts
@@ -1,35 +1,21 @@
-export default function minimumCoinsForChange(coins: number[], target: number) {
-  if (target == 0) return 0;
-  let min = Infinity;
-  let suffix = new Map<number, number>();
-  let sum = 0;
-  for (let i = 0; i < coins.length; i++) {
-    sum += coins[i];
-    suffix.set(sum, i + 1);
-    if (i > 0) {
-      suffix.set(coins[i] + coins[i - 1], 2);
-    }
-    if (i < coins.length - 1) {
-      suffix.set(coins[i] + coins[i + 1], 2);
-    }
-  }
-  if (suffix.has(target)) {
-    min = Math.min(min, suffix.get(target) ?? Infinity);
-  }
-  for (let i = 0; i < coins.length; i++) {
-    const number = coins[i];
-    const remain = target % number;
+export default function minimumCoinsForChange(
+  coins: number[],
+  target: number
+): number {
+  if (target === 0) return 0;
 
-    if (remain == 0) {
-      min = Math.min(min, target / number);
-    } else if (coins.includes(remain)) {
-      min = Math.min(min, (target - remain) / number + 1);
-    } else if (suffix.has(remain)) {
-      min = Math.min(min, (target - remain) / number + suffix.get(remain)!);
+  const dp = new Array(target + 1).fill(Infinity);
+  dp[0] = 0;
+
+  for (let amount = 1; amount <= target; amount++) {
+    for (const coin of coins) {
+      if (amount - coin >= 0) {
+        dp[amount] = Math.min(dp[amount], dp[amount - coin] + 1);
+      }
     }
   }
-  console.log(...suffix.keys());
-  return min == Infinity ? -1 : min;
+  console.log(dp);
+  return dp[target] === Infinity ? -1 : dp[target];
 }
 
-console.log(minimumCoinsForChange([186, 419, 83, 408], 6249));
+console.log(minimumCoinsForChange([1, 2, 5, 10], 27));
