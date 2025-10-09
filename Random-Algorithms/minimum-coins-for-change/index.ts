849b85e66a7867e230a7a10b1c2e0d17d989c3d2
Mobin Afshari <mma137813781378@gmail.com>
2025-10-09 16:59:41 +0330

    fix: still working on minimum coins for change

--- Random-Algorithms/minimum-coins-for-change/index.ts
+++ Random-Algorithms/minimum-coins-for-change/index.ts
@@ -4,10 +4,22 @@ export default function minimumCoinsForChange(coins: number[], target: number) {
   let suffix = new Map<number, number>();
   let sum = 0;
   for (let i = 0; i < coins.length; i++) {
+    sum += coins[i];
+    suffix.set(sum, i + 1);
+    if (i > 0) {
+      suffix.set(coins[i] + coins[i - 1], 2);
+    }
+    if (i < coins.length - 1) {
+      suffix.set(coins[i] + coins[i + 1], 2);
+    }
+  }
+  if (suffix.has(target)) {
+    min = Math.min(min, suffix.get(target) ?? Infinity);
+  }
+  for (let i = 0; i < coins.length; i++) {
     const number = coins[i];
     const remain = target % number;
-    sum += number;
-    suffix.set(sum, i + 1);
+
     if (remain == 0) {
       min = Math.min(min, target / number);
     } else if (coins.includes(remain)) {
@@ -16,8 +28,8 @@ export default function minimumCoinsForChange(coins: number[], target: number) {
       min = Math.min(min, (target - remain) / number + suffix.get(remain)!);
     }
   }
-  console.log([...suffix.keys()]);
+  console.log(...suffix.keys());
   return min == Infinity ? -1 : min;
 }
 
-console.log(minimumCoinsForChange([1, 2, 5, 10], 27));
+console.log(minimumCoinsForChange([186, 419, 83, 408], 6249));
