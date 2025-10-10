6b3b18e2ec04b2a6e0319e967ad6862b3e07df57
Mobinafshari <mma137813781378@gmail.com>
2025-10-10 18:34:13 +0330

    feat: implemented from simple way

--- Trie/index.ts
+++ Trie/index.ts
@@ -1,9 +1,23 @@
 class Trie {
-  constructor() {}
+  words: string[];
+  constructor() {
+    this.words = [];
+  }
 
-  public insert(word: string): void {}
+  public insert(word: string): void {
+    this.words = [...this.words, word];
+  }
 
-  public search(word: string): boolean {}
+  public search(word: string): boolean {
+    return this.words.includes(word);
+  }
 
-  public startsWith(prefix: string): boolean {}
+  public startsWith(prefix: string): boolean {
+    return this.words.some((word) => word.startsWith(prefix));
+  }
 }
+
+const trie = new Trie();
+trie.insert("cat");
+console.log(trie.search("cat"));
+console.log(trie.startsWith("ca"));
