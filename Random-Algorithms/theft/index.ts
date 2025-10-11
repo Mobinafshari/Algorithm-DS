08d9d5e50a57ed0180f925ad2adb1d63bbbc7b10
Mobin Afshari <mma137813781378@gmail.com>
2025-10-11 21:28:46 +0330

    feat: implemented with DI , no idea about it

--- Random-Algorithms/theft/index.ts
+++ Random-Algorithms/theft/index.ts
@@ -3,42 +3,20 @@
 //  the security systems in adjacent houses are interconnected. If the robber breaks into two neighboring houses, the police will be alerted.
 
 // Given an array of integers numbers where each element represents the amount of money in a house, determine the maximum amount of money the robber can steal without triggering the alarm.
-export default function neighborhoodTheft(numbers: number[]): number {
-  let prev = new Map<number, number>();
-  let index = 0;
-  let deleted;
-  for (let i = 0; i < numbers.length; i++) {
-    const number = numbers[i];
-    const now = prev.get(index) ?? 0;
-    if (number > now) {
-      if (i - index == 1) {
-        deleted = {
-          index,
-          number: prev.get(index)!,
-        };
-        console.log("----", deleted, index, i, number);
-        prev.delete(index);
-        // if (deleted.index && deleted.index - i !== 1) {
-        //   prev.set(deleted.index, deleted.number);
-        //   deleted = {};
-        // }
-        index = i;
-        prev.set(i, number);
-      } else {
-        index = i;
-        prev.set(i, number);
-      }
-    } else {
-      if (i - index == 1) {
-        continue;
-      } else {
-        index = i;
-        prev.set(i, number);
-      }
-    }
+function neighborhoodTheft(numbers: number[]): number {
+  if (numbers.length === 0) return 0;
+  if (numbers.length === 1) return numbers[0];
+
+  let prev2 = numbers[0]; 
+  let prev1 = Math.max(numbers[0], numbers[1]);
+
+  for (let i = 2; i < numbers.length; i++) {
+    const current = Math.max(prev1, prev2 + numbers[i]);
+    prev2 = prev1;
+    prev1 = current;
   }
-  console.log(prev);
-  return [...prev.values()].reduce((acc, current) => acc + current, 0);
+
+  return prev1;
 }
 
 console.log(neighborhoodTheft([2, 7, 9, 3, 1]));
