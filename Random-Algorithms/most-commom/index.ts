eae03ed2db933f7f2aa1cd9527940e493a20272d
Mobin Afshari <mma137813781378@gmail.com>
2025-10-11 19:11:27 +0330

    fix: top k frequent elements implemented

--- Random-Algorithms/most-commom/index.ts
+++ Random-Algorithms/most-commom/index.ts
@@ -2,5 +2,19 @@ export default function mostCommonElements(
   numbers: number[],
   k: number
 ): number[] {
-  throw "Not implemented!";
+  const freqMap = new Map<number, number>();
+  for (const number of numbers) {
+    freqMap.set(number, (freqMap.get(number) ?? 0) + 1);
+  }
+  const res = [...freqMap.entries()]
+    .sort((a, b) => {
+      if (b[1] !== a[1]) {
+        return b[1] - a[1];
+      }
+      return a[0] - b[0];
+    })
+    .map((element) => element[0]);
+  return res.slice(0, k);
 }
+
+console.log(mostCommonElements([7, 7, 7, 8, 8, 9, 9, 9, 9, 5, 5, 5], 3));
