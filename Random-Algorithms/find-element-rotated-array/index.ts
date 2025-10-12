2bcf32b4ca1d852c5038f3d732fdc40c651df74d
Mobinafshari <mma137813781378@gmail.com>
2025-10-12 15:12:17 +0330

    fix: implemented

--- Random-Algorithms/find-element-rotated-array/index.ts
+++ Random-Algorithms/find-element-rotated-array/index.ts
@@ -1,8 +1,28 @@
-export default function findInRotatedArray(
-  numbers: number[],
-  target: number
-): number {
-  throw "Not implemented!";
+function findInRotatedArray(numbers: number[], target: number): number {
+  let left = 0;
+  let right = numbers.length - 1;
+
+  while (left <= right) {
+    const middle = Math.floor((left + right) / 2);
+
+    if (numbers[middle] === target) return middle;
+
+    if (numbers[left] <= numbers[middle]) {
+      if (target >= numbers[left] && target < numbers[middle]) {
+        right = middle - 1;
+      } else {
+        left = middle + 1;
+      }
+    } else {
+      if (target > numbers[middle] && target <= numbers[right]) {
+        left = middle + 1;
+      } else {
+        right = middle - 1;
+      }
+    }
+  }
+
+  return -1;
 }
 
-console.log(findInRotatedArray([0, 1, 2, 3, 4], 2));
+console.log(findInRotatedArray([2, 3, 4, 0, 1], 0)); // ✅ Output: 3
