dfc08700958449ba067b7b64c3f2f9d685055b15
Mobinafshari <mma137813781378@gmail.com>
2025-10-12 14:33:46 +0330

    feat: working on random algorithms: find element in rotated array

---
+++ Random-Algorithms/find-element-rotated-array/index.ts
@@ -0,0 +1,8 @@
+export default function findInRotatedArray(
+  numbers: number[],
+  target: number
+): number {
+  throw "Not implemented!";
+}
+
+console.log(findInRotatedArray([0, 1, 2, 3, 4], 2));
