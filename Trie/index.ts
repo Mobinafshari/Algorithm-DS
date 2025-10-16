fdd51a6d6d1cd8b05ae0d7f135f90bdf70643d24
Mobinafshari <mma137813781378@gmail.com>
2025-10-16 22:18:01 +0330

    fix: last changes

--- Trie/index.ts
+++ Trie/index.ts
@@ -1,3 +1,19 @@
-class Trie {
-  
+class TrieNode {
+  children: Map<string, TrieNode>;
+  isLastChar: boolean;
+  constructor() {
+    this.children = new Map();
+    this.isLastChar = false;
+  }
 }
+
+class Trie {
+  public insert(word: string) {
+    new TrieNode(word);
+  }
+  public search(word: string) {}
+  public startsWith(prefix: string) {}
+}
+
+const trie = new Trie();
+console.log(trie.insert("hashem"));
