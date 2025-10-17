75ef668754d653b7a3c72fd468409ded5bac3467
Mobinafshari <mma137813781378@gmail.com>
2025-10-17 10:21:54 +0330

    feat: insert method completed

--- Trie/index.ts
+++ Trie/index.ts
@@ -8,8 +8,24 @@ class TrieNode {
 }
 
 class Trie {
+  root: TrieNode;
+
+  constructor() {
+    this.root = new TrieNode();
+  }
+
   public insert(word: string) {
-    new TrieNode(word);
+    let node = this.root;
+
+    for (const char of word) {
+      if (!node.children.has(char)) {
+        node.children.set(char, new TrieNode());
+      }
+      console.log(node);
+      node = node.children.get(char)!;
+    }
+
+    node.isLastChar = true;
   }
   public search(word: string) {}
   public startsWith(prefix: string) {}
