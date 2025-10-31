bc511d5b398e5f5489a3f0c775eea89ff3dd6ec4
Mobin Afshari <mma137813781378@gmail.com>
2025-10-31 10:31:56 +0330

    feat: working on invert tree algo

--- LCA/index.ts
+++ LCA/index.ts
@@ -44,12 +44,10 @@ function isSameTree(a: TreeNode | null, b: TreeNode | null): boolean {
   if (a.val !== b.val) return false;
   return isSameTree(a.left, b.left) && isSameTree(a.right, b.right);
 }
+
+function invertTree(root: TreeNode | null): TreeNode | null {}
 const root = new TreeNode(
-  3,
-  new TreeNode(4, new TreeNode(1), new TreeNode(2)),
-  new TreeNode(5)
+  4,
+  new TreeNode(2, new TreeNode(1), new TreeNode(3)),
+  new TreeNode(7, new TreeNode(6), new TreeNode(9))
 );
-
-const subRoot = new TreeNode(4, new TreeNode(1), new TreeNode(2));
-
-console.log("--", isSubtree(root, subRoot));
